58743edd26c41f03af6ba40fb3468551537ffee5
Come <56791111+ShikoteiCoding@users.noreply.github.com>
2023-01-27 16:13:58 +0800

    REST API

--- server/app/app.py
+++
@@ -1,8 +0,0 @@
-import os
-from sqlalchemy import create_engine
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-
-if __name__ == "__main__":
-    print("Here")

5dd952c83e50aa7dbd36d45b371d23d1e8235ce7
Come <56791111+ShikoteiCoding@users.noreply.github.com>
2023-01-27 11:16:08 +0800

    Adding a down for faster early project modifs

--- server/down.sh
+++ server/down.sh
@@ -1,3 +1,18 @@
-docker compose down
-docker rm todo-db
-docker volume rm server_todo-db
+#!/bin/sh
+
+CONTAINER_NAME="note-db"
+VOLUME_NAME="server_note-db"
+
+if [ $( docker ps -a | grep $CONTAINER_NAME | wc -l ) -gt 0 ]; then
+    echo "Taking down $CONTAINER_NAME..."
+    docker rm $CONTAINER_NAME
+else
+    echo "Container $CONTAINER_NAME does not exist."
+fi
+
+if [ $( docker volume ls | grep $VOLUME_NAME | wc -l ) -gt 0 ]; then
+    echo "Deleting volume $VOLUME_NAME..."
+    docker volume rm $VOLUME_NAME
+else
+    echo "Volume $VOLUME_NAME does not exist."
+fi

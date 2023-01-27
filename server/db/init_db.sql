899ad753e02d85b4a5176a92c40f6146aaa8a0be
Come <56791111+ShikoteiCoding@users.noreply.github.com>
2023-01-27 11:16:16 +0800

    Change the schema

--- server/db/init_db.sql
+++ server/db/init_db.sql
@@ -1,31 +1,14 @@
-CREATE SEQUENCE list_id_pk_seq
+CREATE SEQUENCE note_id_pk_seq
     START 1
     INCREMENT 1
     NO MAXVALUE
     CACHE 1;
 
-CREATE TABLE IF NOT EXISTS lists (
-    id INT NOT NULL DEFAULT nextval('list_id_pk_seq'),
+CREATE TABLE IF NOT EXISTS notes (
+    id INT NOT NULL DEFAULT nextval('note_id_pk_seq'),
     title TEXT NOT NULL,
+    content TEXT,
     create_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     modify_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
-);
-
-CREATE SEQUENCE item_id_pk_seq
-    START 1
-    INCREMENT 1
-    NO MAXVALUE
-    CACHE 1;
-
-CREATE TABLE IF NOT EXISTS items (
-    id INT NOT NULL DEFAULT nextval('item_id_pk_seq'),
-    content TEXT NOT NULL,
-    create_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    modify_date TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    list_id INT NOT NULL,
-    PRIMARY KEY (id),
-    CONSTRAINT fk_list
-        FOREIGN KEY (list_id) 
-            REFERENCES lists(id)
 );

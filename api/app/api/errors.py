890044a8317750c59883af410e9c72096a74fa9f
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-01-29 18:15:03 +0800

    API HTTP error wrapper

--- api/app/api/errors.py
+++ api/app/api/errors.py
@@ -1,2 +1,21 @@
-def bad_request():
-    pass
+from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+
+
+def error_response(status_code: int, message: str | None = None) -> dict[str, str]:
+    """
+    Generate a JSON error response with specified HTTP status code and message.
+    :param status_code: HTTP status code
+    :param message: Custom error message.
+    :return: JSON error response with set status code and message.
+    """
+    payload = {"error": HTTP_STATUS_CODES.get(status_code, "Unknown error")}
+    if message:
+        payload["message"] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response  # type: ignore
+
+
+def bad_request(message):
+    return error_response(400, message)

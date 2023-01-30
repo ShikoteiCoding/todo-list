70e562ad1fff54bcba6d300aa3107ba28f381171
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-01-30 20:44:31 +0800

    Rewriting config

--- api/config.py
+++ api/config.py
@@ -14,17 +14,15 @@ def getenv_required(name: str) -> str:
     return var
 
 
-class Config(object):
+def sqlalchemi_database_uri() -> str:
     POSTGRES_USER = getenv_required("POSTGRES_USER")
     POSTGRES_PASSWORD = getenv_required("POSTGRES_PASSWORD")
     POSTGRES_HOST = getenv_required("POSTGRES_EXTERNAL_HOST")
     POSTGRES_PORT = getenv_required("POSTGRES_PORT")
     POSTGRES_DB = getenv_required("POSTGRES_DB")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
+    return f"postgres://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        if utils.is_docker():
-            return f"postgres://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
-        return "sqlite:///" + "app.db"  # macos uri
+
+class Config(object):
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = sqlalchemi_database_uri()

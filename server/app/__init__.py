f8fa00007a4ea9459bbe91fbf2fc2a4d76c1070b
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-06 21:21:15 +0800

    Fixing env not load

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+import os
+
 from .config import BaseConfig
 
 db = SQLAlchemy()
@@ -13,8 +15,12 @@ login = LoginManager()
 def create_app(*, config: BaseConfig | None = None) -> Flask:
     app = Flask(__name__)
 
+    # set config
     if config:
         app.config.from_object(config)
+    else:
+        app_settings = os.getenv("APP_SETTINGS")
+        app.config.from_object(app_settings)
 
     db.init_app(app)
     migrate.init_app(app, db)

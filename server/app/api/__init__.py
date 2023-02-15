30c7a33d58880510acc0acc71e9c4bca2e281ce3
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-15 21:30:33 +0800

    Fkask restx setup

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -1,5 +1,10 @@
 from flask import Blueprint
+from flask_restx import Api
 
 bp = Blueprint("api", __name__)
 
-from app.api import users, errors, tokens
+from app.api._users.views import user_namespace
+from app.api import users
+
+api = Api(version="1.0", title="Flask API", doc="/api/v1/docs")
+api.add_namespace(user_namespace, path="/api/v1/movies")

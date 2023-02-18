40688377d56cc1b7365864a614febf507e256c2d
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-18 10:32:46 +0800

    User delete endpoints test

--- server/app/tests/test_user.py
+++ server/app/tests/test_user.py
@@ -34,6 +34,7 @@ def test_user_get_200(app: Flask, database: SQLAlchemy, add_user: Callable) -> N
     _user = add_user("User_200")
     client = app.test_client()
     response = client.get(f"/api/v1/users/{_user.id}", content_type="application/json")
+
     assert response.status_code == 200
     assert response.content_type == "application/json"
 
@@ -44,6 +45,7 @@ def test_user_get_404(app: Flask, database: SQLAlchemy) -> None:
     client = app.test_client()
     response = client.get("/api/v1/users/111", content_type="application/json")
     data = json.loads(response.data.decode())
+
     assert response.status_code == 404
     assert "user does not exist" in data["message"]
 
@@ -71,5 +73,26 @@ def test_user_put_404(app: Flask, database: SQLAlchemy) -> None:
         data=json.dumps({"username": "User_Put_200"}),
     )
     data = json.loads(response.data.decode())
+
+    assert response.status_code == 404
+    assert "user does not exist" in data["message"]
+
+
+def test_user_delete_204(app: Flask, database: SQLAlchemy, add_user: Callable) -> None:
+    """UserDetail.DELETE - 204"""
+    _user = add_user("User_Delete")
+    client = app.test_client()
+    response = client.delete(f"/api/v1/users/{_user.id}")
+
+    assert response.status_code == 204
+    assert response.content_type == "application/json"
+
+
+def test_user_delete_404(app: Flask, database: SQLAlchemy) -> None:
+    """UserDetail.DELETE - 204"""
+    client = app.test_client()
+    response = client.delete(f"/api/v1/users/111")
+    data = json.loads(response.data.decode())
+
     assert response.status_code == 404
     assert "user does not exist" in data["message"]

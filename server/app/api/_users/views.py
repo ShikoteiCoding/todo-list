0a2c659434e7fc19b3f2892ea8df0fd72083bc30
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-18 09:45:34 +0800

    User view endpoint

--- server/app/api/_users/views.py
+++ server/app/api/_users/views.py
@@ -5,7 +5,7 @@ from app.api._users.crud import (
     get_user_by_id,
     create_user,
     update_user,
-    delete_user
+    delete_user,
 )
 
 from app.api._users.serializer import post_user_serializer
@@ -19,9 +19,10 @@ user = users_namespace.model(
     {
         "id": fields.Integer(readOnly=True),
         "username": fields.String(required=True),
-        "token": fields.String(readOnly=True)
-    }
+        "token": fields.String(readOnly=True),
+    },
 )
+
 
 class UserList(Resource):
     @users_namespace.marshal_with(user, as_list=True)
@@ -40,19 +41,40 @@ class UserList(Resource):
         args = post_user_serializer.parse_args()
         return create_user(args["username"]), 201
 
-#class UserDetail(Resource):
-#    @users_namespace.marshal_with(user)
-#    def get(self):
-#        """returns all users"""
-#
-#        print("UserList.GET")
-#        return get_all_users(), 200
-#
-#    @users_namespace.expect(post_user_serializer, validate=True)
-#    @users_namespace.marshal_with(user)
-#    def post(self):
-#        """returns a single user"""
-#
-#        print("UserList.POST")
-#        args = post_user_serializer.parse_args()
-#        return create_user(args["username"]), 201
+
+class UserDetail(Resource):
+    @users_namespace.marshal_with(user)
+    def get(self, user_id: int):
+        """returns a single user"""
+
+        print("UserDetail.GET")
+        user = get_user_by_id(user_id)
+        if not user:
+            users_namespace.abort(404, "user does not exist")
+        return user, 200
+
+    @users_namespace.expect(post_user_serializer, validate=True)
+    @users_namespace.marshal_with(user)
+    def put(self, user_id: int):
+        """updates a single user"""
+
+        print("UserDetail.PUT")
+        args = post_user_serializer.parse_args()
+        user = get_user_by_id(user_id)
+        if not user:
+            users_namespace.abort(404, "user does not exist")
+        return update_user(user, args["username"]), 200
+
+    @users_namespace.marshal_with(user)
+    def delete(self, user_id: int):
+        """deletes a single user"""
+
+        print("UserDetail.DELETE")
+        user = get_user_by_id(user_id)
+        if not user:
+            users_namespace.abort(404, "user does not exist")
+        return delete_user(user), 204
+
+
+users_namespace.add_resource(UserList, "")
+users_namespace.add_resource(UserDetail, "/<int:user_id>")

468a1e57df398ea2c7fc59902474483942373945
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-18 09:46:19 +0800

    Fixing init

--- server/app/api/__init__.py
+++ server/app/api/__init__.py
@@ -3,8 +3,8 @@ from flask_restx import Api
 
 bp = Blueprint("api", __name__)
 
-from app.api._users.views import user_namespace
+from app.api._users.views import users_namespace
 from app.api import users
 
 api = Api(version="1.0", title="Flask API", doc="/api/v1/docs")
-api.add_namespace(user_namespace, path="/api/v1/movies")
+api.add_namespace(users_namespace, path="/api/v1/movies")

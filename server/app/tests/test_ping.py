6cbe9cc84f2949fc5ae043c26315abeee8ae27ad
Come Vitis <56791111+ShikoteiCoding@users.noreply.github.com>
2023-02-25 19:11:54 +0800

    Re organisation

--- server/app/tests/test_ping.py
+++ server/app/tests/test_ping.py
@@ -8,6 +8,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
+def test_ping_200(app: Flask, database: SQLAlchemy, add_user_with_token: Callable):
+    """UserList.GET"""
+
+    _user = add_user_with_token("Ping_403", "Ping_403")
+    client = app.test_client()
+    response = client.get(
+        "/api/v1/ping",
+        data=json.dumps(
+            {
+                "username": _user.username,
+                "api_key": _user.token,
+            }
+        ),
+        content_type="application/json",
+    )
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 200
+    assert response.content_type == "application/json"
+    assert "ping" in data["message"]
+
+
 def test_ping_400(app: Flask, database: SQLAlchemy):
     """UserList.GET - 400"""
 
@@ -34,25 +56,3 @@ def test_ping_403(app: Flask, database: SQLAlchemy):
 
     assert response.status_code == 403
     assert response.content_type == "application/json"
-
-
-def test_ping_200(app: Flask, database: SQLAlchemy, add_user_with_token: Callable):
-    """UserList.GET"""
-
-    _user = add_user_with_token("Ping_403", "Ping_403")
-    client = app.test_client()
-    response = client.get(
-        "/api/v1/ping",
-        data=json.dumps(
-            {
-                "username": _user.username,
-                "api_key": _user.token,
-            }
-        ),
-        content_type="application/json",
-    )
-    data = json.loads(response.data.decode())
-
-    assert response.status_code == 200
-    assert response.content_type == "application/json"
-    assert "ping" in data["message"]
